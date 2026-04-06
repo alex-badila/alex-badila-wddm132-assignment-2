eb3ce448f31861068dff1abef5fdf4beae94c851
alex-badila <24686529+alex-badila@users.noreply.github.com>
2026-04-06 18:39:45 -0400

    Changed the tags for header and footer to be more descriptive

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer>
       <p>Copyright &copy; 2026</p>
-    </div>
+    </footer>
   );
 }
 
--- app/header.tsx
+++ app/header.tsx
@@ -2,19 +2,21 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="navContainer">
-      <div className="navLeft">
-        <Link href="/">The Leaky Goat</Link>
+    <header>
+      <div className="navContainer">
+        <div className="navLeft">
+          <Link href="/">The Leaky Goat</Link>
+        </div>
+        <div className="navCenter">
+          <Link href="/lunch">Lunch</Link>
+          <Link href="/dinner">Dinner</Link>
+          <Link href="/drinks">Drinks</Link>
+        </div>
+        <div className="navRight">
+          <Link href="/reservation">Reservation</Link>
+        </div> 
       </div>
-      <div className="navCenter">
-        <Link href="/lunch">Lunch</Link>
-        <Link href="/dinner">Dinner</Link>
-        <Link href="/drinks">Drinks</Link>
-      </div>
-      <div className="navRight">
-        <Link href="/reservation">Reservation</Link>
-      </div> 
-    </div>
+    </header>
   );
 }
 
